8f849facf9a7d7f02bcce59ceecd2a1e1ab5094c
Raul Ţifrea <raoul_tifrea@yahoo.com>
2020-01-21 17:00:59 +0200

    Edited Defs class Added basic functionality to Transilvania script

---
+++ Digi24/Pages/Digi24_meteo.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+import unittest, os, datetime
+
+from selenium.webdriver.common.by import By
+
+class Defs:
+    URL = 'https://www.digi24.ro/meteo'
+    GDPR = (By.XPATH, ".//*[@class='gdpr-button gdpr-trigger']")
+    Center_Element = (By.XPATH, ".//*[@class='weather-map-pin weather-map-pin-sibiu")
+    Pressure = (By.CLASS_NAME, "atmo")
+    City = (By.CLASS_NAME, "city")
+    Wind = (By.CLASS_NAME, "wind")
+    Temperature = (By.CLASS_NAME,"temp")
+    #To add loop through cities
+
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+
+    def center_element(self):
+        actions = ActionChains(self.driver)
+        actions.move_to_element(self.Center_Element).perform()
+
+    def load(self):
+        self.driver.maximize_window()
+        self.driver.get(self.URL)
+        self.driver.find_element(*self.GDPR).click()
+
+
+
+
+
+
