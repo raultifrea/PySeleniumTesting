3147bfa7ed0014e163c0d21778c5f7f706331ee7
Raul Ţifrea <raoul_tifrea@yahoo.com>
2020-02-06 16:40:52 +0200

    modified stuff

--- Digi24/Pages/Meteo_Romania.py
+++ Digi24/Pages/Meteo_Romania.py
@@ -2,7 +2,10 @@
 from selenium import webdriver
 import datetime
 
+from selenium.common.exceptions import StaleElementReferenceException, TimeoutException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class Defs:
     URL = 'https://www.digi24.ro/meteo'
@@ -11,7 +14,9 @@ class Defs:
     City_locator = (By.CLASS_NAME, "city")
     Wind_locator = (By.CLASS_NAME, "wind")
     Temperature_locator = (By.CLASS_NAME,"temp")
-    Cities_descendant_locator = (By.XPATH, ".//*[@class ='weather-map weather-map-romania']/descendant::button")
+    Cities_descendant_locator_Romania = (By.XPATH, ".//*[@class ='weather-map weather-map-romania']/descendant::button")
+    Cities_descendant_locator_Transilvania = (By.XPATH, ".//*[@class ='weather-map weather-map-transilvania']/descendant::button")
+    Regions_descendant_locator =(By.XPATH, ".//*[@class='dropdown-list-item']/following-sibling::li")
 
     def __init__(self):
         self.driver = webdriver.Chrome()
@@ -27,6 +32,19 @@ class Defs:
         self.driver.maximize_window()
         self.driver.get(self.URL)
         self.driver.find_element(*self.GDPR_locator).click()
+
+    def get_Regions(self, region):
+        list_of_regions = self.driver.find_elements(*self.Regions_descendant_locator)
+        for element in list_of_regions:
+            if region == element.text:
+                element.click()
+                break
+
+    def get_Cities_descendants(self): #placeholder argument for the pressures,temperatures,winds methods
+        list_of_elements = self.driver.find_elements(*self.Cities_descendant_locator_Romania)
+        for element in list_of_elements:
+            element.click()
+            self.pressures()
 
     def mmHg_to_psi(self, x):  # converting given atm pressure from mmHg to psi
         formula = int(x) / 51.715
@@ -48,11 +66,7 @@ class Defs:
                 print(format(self.city, '*^50'), file=file)
 
     def run_pressures(self):
-        list_of_elements = self.driver.find_elements(*self.Cities_descendant_locator)
-        for element in list_of_elements:
-            element.click()
-            self.pressures()
-
+        self.get_Cities_descendants()
         self.pressures_list = [float(x) for x in self.pressures_list]
         self.pressure_avg = sum(self.pressures_list) / len(self.pressures_list)
         print("\nMedia presiunii atmosferice din Romania este " + str(("%.2f" % round(self.pressure_avg, 2))) + " psi\n")
@@ -60,7 +74,7 @@ class Defs:
             print("\nMedia presiunii atmosferice din Romania este " + str(("%.2f" % round(self.pressure_avg, 2))) + " psi\n", file=file)
 
     def temperatures(self):
-        self.temperature_value =self.driver.find_element(*self.Temperature_locator).text
+        self.temperature_value = self.driver.find_element(*self.Temperature_locator).text
         self.city = self.driver.find_element(*self.City_locator).text
         print("Temperatura din " + self.city + " este " + self.temperature_value + "°C")
         with open(self.current_day + ".txt", "a+", encoding="utf-8") as file:
@@ -73,11 +87,7 @@ class Defs:
                 print(format(self.city, '*^50'), file=file)
 
     def run_temperatures(self):
-        list_of_elements = self.driver.find_elements(*self.Cities_descendant_locator)
-        for element in list_of_elements:
-            element.click()
-            self.temperatures()
-
+        self.get_Cities_descendants()
         self.temperatures_list = [int(x) for x in self.temperatures_list]
         self.temp_avg = sum(self.temperatures_list) / len(self.temperatures_list)
         print("\nMedia temperaturii pe Romania este " + str(("%.2f" % round(self.temp_avg, 2))) + "°C\n")
@@ -105,11 +115,7 @@ class Defs:
             print(format(self.city, '*^50'))
 
     def run_winds(self):
-        list_of_elements = self.driver.find_elements(*self.Cities_descendant_locator)
-        for element in list_of_elements:
-            element.click()
-            self.winds()
-
+        self.get_Cities_descendants()
         self.winds_list = [float(x) for x in self.winds_list]
         self.wind_avg = sum(self.winds_list) / len(self.winds_list)
         print("\nMedia vitezei vantului in Romania este " + str(("%.2f" % round(self.wind_avg, 2))) + " km\h\n")
---
+++ Digi24/Tests/Test_Meteo_Pressure_Transilvania.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+import unittest, os, datetime
+
+from Digi24.Pages.Meteo_Romania import Defs
+
+
+class TestMeteoPressureTransilvania(unittest.TestCase):
+
+    #os.chdir(r'C:\Users\tzifrea\Desktop\Meteo\Transilvania')
+    os.chdir(r'C:\Users\F73482\Desktop\Meteo\Transilvania')
+
+    meteo_transilvania = Defs()
+    meteo_transilvania.load()
+    #meteo_transilvania.center_element()
+    meteo_transilvania.get_Regions("TRANSILVANIA")
+    meteo_transilvania.run_pressures()
+    meteo_transilvania.quit()
+
+if __name__ == "__main__":
+    unittest.main()
+
+
