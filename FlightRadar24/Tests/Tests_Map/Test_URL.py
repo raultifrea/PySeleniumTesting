1519ed1f2a693a8b144d33a5126fdc0006455a19
Raul Tifrea <raul.tifrea@porsche-engineering.ro>
2021-09-14 10:50:12 +0300

    left only Digi24 tests on this branch

--- FlightRadar24/Tests/Tests_Map/Test_URL.py
+++
@@ -1,9 +0,0 @@
-from FlightRadar24.Pages.Map import Defs
-import unittest
-
-class TestURL(unittest.TestCase):
-
-    def testURL(self):
-        test = Defs()
-        test.load()
-        test.quit()
